a0cc168fa5bb8fc4b95fd71eddfa518df913447d
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-02 19:00:54 -0700

    psuhing new adjusted code for api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,79 +1,70 @@
 $(document).ready(function() {
+  var client_id = 'a0cbb93ccba44ab8b9c2ad70242833ee'; // Replace with your actual Spotify client ID
+  var redirect_Uri = 'https://www.google.com'; // Replace with your redirect URI
+  var scope = 'user-read-private user-read-email'; // Specify the required scopes
+  var authCode = getQueryParam('code'); // Get the authorization code from the query parameters
+  var accessToken = '';
 
+  // Function to extract query parameters from the URL
+  function getQueryParam(param) {
+      var urlParams = new URLSearchParams(window.location.search);
+      return urlParams.get(param);
+  }
 
-
-var client_id = 'CLIENT ID';
-var redirect_Uri = 'www.google.com'; //needs to be changed when we know our websites name
-var scope = 'user-read-private user-read-email'; // Specify the required scopes need to figure out scopes we need
-var authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${encodeURIComponent(redirect_Uri)}&scope=${encodeURIComponent(scope)}`;
-var clientSecret = 'YOUR_CLIENT_SECRET';
-var authCode = "";
-var accessToken = "";
-
-
-//back to authoirzation URL
-window.location.href = authorizeUrl;//will take user to login to spotify and grat permission
-var authCode = "AUTHORIZATION_CODE" //AUTH FROM REDIRECT URL need to figure out how to get this
-//extraction of auth code should preferably done after bing redirected
-var tokenUrl = 'https://accounts.spotify.com/api/token';
-
-
-
- //define data object for the token request as spotify api uses
-var data = {
-    grant_type: 'authorization_code',
-    code: authCode, //needs actual code for auth
-    redirect_uri: redirect_Uri,
-  };
-
-  
-  var headers = {
-    'Authorization': `Basic ${btoa(`${client_id}:${clientSecret}`)}`,
-    'Content-Type': 'application/x-www-form-urlencoded',
-  };
+  // Check if an authorization code is present in the query parameters
   if (!authCode) {
-    console.error('Authorization code not found.');
+      // Redirect the user to the Spotify authorization URL
+      var authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${encodeURIComponent(redirect_Uri)}&scope=${encodeURIComponent(scope)}`;
+      window.location.href = authorizeUrl; // Redirect the user to Spotify for authorization
   } else {
-    var tokenUrl = 'https://accounts.spotify.com/api/token';
-  //method is used to request the token to enpoint  token to exchange auth code for auth token
-  //post request which sends token for exchange of access/authorization
-  fetch(tokenUrl, {
-    method: 'POST',
-    body: new URLSearchParams(data),
-    headers: headers,
-  })
-  //preps for the request to go through. parse JSON then if successful it will set our token and expiresIn variables.
-    .then(response => response.json())
-    .then(data => {
-      accessToken = data.access_token;
-      /*possibly store these for future requests in case of timeout??????*/
+      // You have obtained the authorization code, now exchange it for an access token
+      var tokenUrl = 'https://accounts.spotify.com/api/token';
 
+      // Define data object for the token request
+      var data = {
+          grant_type: 'authorization_code',
+          code: authCode,
+          redirect_uri: redirect_Uri,
+      };
 
-      var apiUrl = 'https://api.spotify.com/v1/me';
+      // Define headers for the token request
+      var headers = {
+          'Authorization': `Basic ${btoa(`${client_id}:${clientSecret}`)}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+      };
 
-      fetch(apiUrl, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`,
-        },//access token obtained is in the header as Authorization
-        //then gewt be processed as needed
+      // Make a POST request to exchange the authorization code for an access token
+      fetch(tokenUrl, {
+          method: 'POST',
+          body: new URLSearchParams(data),
+          headers: headers,
       })
       .then(response => response.json())
       .then(data => {
-        // this will take the API response
-        console.log(data);
-      })
+          accessToken = data.access_token;
 
-      // Use the access token to make requests to the Spotify API
-      // Store the access token and expiration time as needed
-  
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  })
+          // Use the access token to make requests to the Spotify API
+          var apiUrl = 'https://api.spotify.com/v1/me';
+
+          fetch(apiUrl, {
+              headers: {
+                  'Authorization': `Bearer ${accessToken}`,
+              },
+          })
+          .then(response => response.json())
+          .then(data => {
+              // Handle the API response (e.g., display user data)
+              console.log(data);
+          })
+          .catch(error => {
+              console.error('Error:', error);
+          });
+      })
       .catch(error => {
-        console.error('Error:', error);
+          console.error('Error:', error);
       });
-    }
-    });
+  }
+});
+
 
     
