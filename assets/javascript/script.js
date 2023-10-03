fcfbf5345e8b02fb62f33923f167dc200a6a457b
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2023-10-02 21:13:08 -0700

    adding file to override javascript that was there previously

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,70 +1,23 @@
-$(document).ready(function() {
-  var client_id = 'a0cbb93ccba44ab8b9c2ad70242833ee'; // Replace with your actual Spotify client ID
-  var redirect_Uri = 'https://www.google.com'; // Replace with your redirect URI
-  var scope = 'user-read-private user-read-email'; // Specify the required scopes
-  var authCode = getQueryParam('code'); // Get the authorization code from the query parameters
-  var accessToken = '';
 
-  // Function to extract query parameters from the URL
-  function getQueryParam(param) {
-      var urlParams = new URLSearchParams(window.location.search);
-      return urlParams.get(param);
-  }
+(async() => {
+var url = 'https://api-basketball.p.rapidapi.com/statistics?season=2019-2020&league=12&team=133';
+var options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3658930d88msh914f6e850e00bbcp1478ddjsnc67b9db401a9',
+		'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
+	}
+};
 
-  // Check if an authorization code is present in the query parameters
-  if (!authCode) {
-      // Redirect the user to the Spotify authorization URL
-      var authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${encodeURIComponent(redirect_Uri)}&scope=${encodeURIComponent(scope)}`;
-      window.location.href = authorizeUrl; // Redirect the user to Spotify for authorization
-  } else {
-      // You have obtained the authorization code, now exchange it for an access token
-      var tokenUrl = 'https://accounts.spotify.com/api/token';
-
-      // Define data object for the token request
-      var data = {
-          grant_type: 'authorization_code',
-          code: authCode,
-          redirect_uri: redirect_Uri,
-      };
-
-      // Define headers for the token request
-      var headers = {
-          'Authorization': `Basic ${btoa(`${client_id}:${clientSecret}`)}`,
-          'Content-Type': 'application/x-www-form-urlencoded',
-      };
-
-      // Make a POST request to exchange the authorization code for an access token
-      fetch(tokenUrl, {
-          method: 'POST',
-          body: new URLSearchParams(data),
-          headers: headers,
-      })
-      .then(response => response.json())
-      .then(data => {
-          accessToken = data.access_token;
-
-          // Use the access token to make requests to the Spotify API
-          var apiUrl = 'https://api.spotify.com/v1/me';
-
-          fetch(apiUrl, {
-              headers: {
-                  'Authorization': `Bearer ${accessToken}`,
-              },
-          })
-          .then(response => response.json())
-          .then(data => {
-              // Handle the API response (e.g., display user data)
-              console.log(data);
-          })
-          .catch(error => {
-              console.error('Error:', error);
-          });
-      })
-      .catch(error => {
-          console.error('Error:', error);
-      });
-  }
-});
+try {
+	var response = await fetch(url, options);
+	var data = await response.json();
+	console.log(data);
+} catch (error) {
+	console.error(error);
+}})()
 
 
-    
+/* test to go off of*/
+
+
